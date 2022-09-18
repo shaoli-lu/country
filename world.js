749f8fe5c48439cf6e9052c92a81b30af24c4fc7
shaoli-lu <shaoli_lu@yahoo.com>
2022-09-18 12:22:41 -0500

    initial

---
+++ world.js
@@ -0,0 +1,43 @@
+let pic = document.getElementById('pic');
+/* 
+document.addEventListener('DOMContentLoaded', function() {
+showCountry();
+
+}) */
+
+function showCountry() 
+{
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://restcountries.com/v3.1/all', true);
+    xhr.onload = function() 
+    {
+        if (xhr.status == 200) 
+        {
+            let countries = JSON.parse(this.response);
+            // countries.push().sort;
+            countries.forEach(  country => 
+            {
+                const countryCard = document.createElement('div');
+                const countryCardImage = document.createElement('img');
+                countryCard.innerHTML =  
+                
+                '</br>' + 'English: ' +country.name.common + 
+                // '</br>' + 'Chinese: ' + country.translations.zho.common + 
+                '</br>' + 'Capital: ' + country.capital + 
+                // '</br>' + 'Language: ' + country.languages + 
+                '</br>' + 'Population: ' + country.population.toLocaleString("en-US") + 
+                '</br>';
+                countryCardImage.src = country.flags.png;
+                countryCard.appendChild(countryCardImage);
+                document.getElementById('feed').appendChild(countryCard);
+            
+            });
+        }
+    }
+xhr.send();
+}
+
+pic.addEventListener('click', showCountry)
+
+
+ 
