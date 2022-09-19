b3dbbd0e59be0373bd59044e558f29ecc86f42d0
shaoli-lu <shaoli_lu@yahoo.com>
2022-09-18 20:16:19 -0500

    sort

--- world.js
+++ world.js
@@ -15,12 +15,21 @@ function showCountry()
         {
             let countries = JSON.parse(this.response);
             // countries.push().sort;
+            
+            countries.sort(function (a, b) {
+                return a.name.common.localeCompare(b.name.common);
+            });
             countries.forEach(  country => 
             {
                 const countryCard = document.createElement('div');
                 const countryCardImage = document.createElement('img');
                 countryCardImage.id ='countryCardImage';
-                if (country.translations.zho == undefined) { zw = '中国';} else { zw = country.translations.zho.common; }
+                if (country.translations.zho !== undefined) {zw = country.translations.zho.common;  } 
+                else if (country.name.common =='Singapore') {zw = '新加坡';}
+                else if (country.name.common =='Taiwan') {zw = '台湾';}
+                else if (country.name.common =='Hong Kong') {zw = '香港';}
+                else if (country.name.common =='Macau') {zw = '澳门';}
+                else {zw = '中国'; }
                 
                 countryCard.innerHTML =  
                 '</br>' + 'Name: ' +country.name.common + 
